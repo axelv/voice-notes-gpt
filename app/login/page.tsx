889cc7eb4ae8cc8c8606848b790698abf6979247
Axel Vanraes <axel.vanraes@tiro.health>
2024-05-24 22:29:23 +0200

    added welcome user page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/client";
 import { Auth } from "@supabase/auth-ui-react";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+const supabase = createClient();
 
 export default function Login() {
   return <Auth supabaseClient={supabase} providers={["notion"]} />;
