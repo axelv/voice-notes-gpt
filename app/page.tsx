2cce4cee9f14ff150524939b1eb87302827326b1
Axel Vanraes <axel.vanraes@tiro.health>
2024-05-24 22:57:17 +0200

    attemt to fix 'outside request' issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import Image from "next/image";
-const supabase = createClient();
 export default async function Home() {
+  const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
