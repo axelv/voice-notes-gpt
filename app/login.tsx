ea26459f22884888391e8c5cef0b404e55205d7e
Axel Vanraes <axel.vanraes@tiro.health>
2024-05-24 22:02:22 +0200

    added login

---
+++ app/login.tsx
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
+
+export default function Login() {
+  return <Auth supabaseClient={supabase} />;
+}
