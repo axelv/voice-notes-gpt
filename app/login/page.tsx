76f3016ddeb32cb0d760d353088319346154a1ee
Axel Vanraes <axel.vanraes@tiro.health>
2024-05-25 12:20:08 +0200

    improve styling of ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { createClient } from "@/utils/supabase/client";
 import { Auth } from "@supabase/auth-ui-react";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
 
 const supabase = createClient();
 export default function Login() {
   return (
     <Auth
+      appearance={{ theme: ThemeSupa }}
       supabaseClient={supabase}
       providers={["notion"]}
       redirectTo="https://voice-notes-gpt.vercel.app/auth/callback"
