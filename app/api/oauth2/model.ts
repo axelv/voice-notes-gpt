9b6f8b15d2f72eecdd9c496af2e01a3eafaf43c9
Axel Vanraes <axel.vanraes@tiro.health>
2024-06-02 17:12:51 +0200

    fix auth for createClient

--- app/api/oauth2/model.ts
+++ app/api/oauth2/model.ts
@@ -1,5 +1,5 @@
 import { Database } from "@/app/database.types";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/server";
 import {
   Client,
   Token,
@@ -21,10 +21,7 @@ if (
   );
 }
 
-const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-);
+const supabase = createClient();
 
 class AuthorizationError extends Error {
   constructor(message: string) {
